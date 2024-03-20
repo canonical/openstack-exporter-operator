7ba34fc6ef8135ba9efe3d1fee48769424135e99
Ray Chan <72372217+chanchiwai-ray@users.noreply.github.com>
2024-03-20 10:07:22 +0800

    Snap integration (#7)
    * Add snap library.

--- src/charm.py
+++ src/charm.py
@@ -9,11 +9,34 @@ OpenStack deployment.
 """
 
 import logging
-from typing import Any
+import os
+from pathlib import Path
+from typing import Any, Optional
 
 import ops
+from ops.model import (
+    ActiveStatus,
+    BlockedStatus,
+    ModelError,
+)
+from service import get_installed_snap_service, snap_install
 
 logger = logging.getLogger(__name__)
+
+# Miscellaneous constants
+USER_HOME = Path().home()
+
+# charm global constants
+RESOURCE_NAME = "openstack-exporter"
+
+# snap global constants
+SNAP_NAME = "golang-openstack-exporter"
+SNAP_SERVICE_NAME = "service"
+
+
+# Snap config options global constants
+CLOUD_NAME = "openstack"
+OS_CLIENT_CONFIG = USER_HOME / "clouds.yaml"
 
 
 class OpenstackExporterOperatorCharm(ops.CharmBase):
@@ -22,6 +45,83 @@ class OpenstackExporterOperatorCharm(ops.CharmBase):
     def __init__(self, *args: tuple[Any]) -> None:
         """Initialize the charm."""
         super().__init__(*args)
+
+        self.framework.observe(self.on.install, self._on_install)
+        self.framework.observe(self.on.upgrade_charm, self._on_upgrade)
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.collect_unit_status, self._on_collect_unit_status)
+
+    def get_resource(self) -> Optional[str]:
+        """Return the path-to-resource or None if the resource is empty.
+
+        Fetch the charm's resource and check if the resource is an empty file
+        or not. If it's empty, return None. Otherwise, return the path to the
+        resource.
+        """
+        try:
+            snap_path = self.model.resources.fetch(RESOURCE_NAME).absolute()
+        except ModelError:
+            logger.warning("cannot fetch charm resource")
+            return None
+
+        if not os.path.getsize(snap_path) > 0:
+            logger.warning("resource is an empty file")
+            return None
+
+        return snap_path
+
+    def install(self) -> None:
+        """Install or upgrade charm."""
+        resource = self.get_resource()
+        if not resource:
+            raise ValueError("resource is invalid or not found.")
+        snap_install(resource, SNAP_SERVICE_NAME)
+
+    def configure(self, event: ops.HookEvent) -> None:
+        """Configure the charm."""
+        snap_service = get_installed_snap_service(SNAP_NAME, SNAP_SERVICE_NAME)
+
+        if not snap_service:
+            logger.warning("snap is not installed, defer configuring the charm.")
+            event.defer()
+            return
+
+        snap_service.configure(
+            {
+                "cloud": CLOUD_NAME,
+                "os-client-config": str(OS_CLIENT_CONFIG),
+                "web": {"listen-address": f":{self.model.config['port']}"},
+            }
+        )
+
+        # TODO: properly start and stop the service depends on the relations to
+        # keystone and grafana-agent.
+        snap_service.start()
+
+    def _on_install(self, _: ops.InstallEvent) -> None:
+        """Handle install charm event."""
+        self.install()
+
+    def _on_upgrade(self, _: ops.UpgradeCharmEvent) -> None:
+        """Handle upgrade charm event."""
+        self.install()
+
+    def _on_config_changed(self, event: ops.ConfigChangedEvent) -> None:
+        """Handle config changed event."""
+        self.configure(event)
+
+    def _on_collect_unit_status(self, event: ops.CollectStatusEvent) -> None:
+        """Handle collect unit status event (called after every event)."""
+        snap_service = get_installed_snap_service(SNAP_NAME, SNAP_SERVICE_NAME)
+
+        if not snap_service:
+            event.add_status(BlockedStatus("snap service is not installed, please check snap service"))
+        elif not snap_service.is_active():
+            event.add_status(
+                BlockedStatus("snap service is not running, please check snap service")
+            )
+
+        event.add_status(ActiveStatus())
 
 
 if __name__ == "__main__":  # pragma: nocover
---
+++ src/service.py
@@ -0,0 +1,75 @@
+"""Utility module to help manage the snap service with guarding functions."""
+
+from logging import getLogger
+from typing import Any, Optional
+
+from charms.operator_libs_linux.v2 import snap
+
+logger = getLogger(__name__)
+
+
+class SnapService:
+    """A class representing the a snap service (only support one service)."""
+
+    def __init__(self, client: snap.Snap, snap_service: str) -> None:
+        """Initialize the class."""
+        self.snap_client = client
+        self.snap_service = snap_service
+
+    def is_active(self) -> bool:
+        """Return True if the snap service is active."""
+        return self.snap_client.services.get(self.snap_service, {}).get("active", False)
+
+    def start(self) -> None:
+        """Start and enable the snap service."""
+        self.snap_client.start([self.snap_service], enable=True)
+
+    def stop(self) -> None:
+        """Stop and disable the snap service."""
+        self.snap_client.stop([self.snap_service], disable=True)
+
+    def configure(self, snap_config: dict[str, Any]) -> None:
+        """Configure the snap service."""
+        # Bail out or it will crash on self.snap_client.set() method
+        if not snap_config:
+            logger.warning("empty snap config: %s, skipping...", snap_config)
+            return
+
+        # Store is previous state, for later use.
+        previously_active = self.is_active()
+
+        self.snap_client.set(snap_config, typed=True)
+
+        # Changing snap configuration will also restart the snap service, so we
+        # need to explicitly preserve the state of the snap service.
+        if not previously_active:
+            self.stop()
+
+
+def snap_install(resource: str, snap_service: str) -> Optional[SnapService]:
+    """Install or refresh the snap, and return the snap service."""
+    try:
+        logger.debug("installing snap.")
+        logger.debug("fetching from resource.")
+        snap_client = snap.install_local(resource, dangerous=True)
+    except snap.SnapError as e:
+        logger.error("failed to install snap: %s", str(e))
+        raise e  # need to crash on_install event if it's not okay
+    else:
+        logger.info("installed %s snap.", snap_client.name)
+        return SnapService(snap_client, snap_service)
+
+
+def get_installed_snap_service(snap_name: str, snap_service: str) -> Optional[SnapService]:
+    """Return the snap service of the snap if it's installed."""
+    try:
+        snap_client = snap.SnapCache()[snap_name]
+    except snap.SnapNotFoundError as e:
+        logger.warning("unable to get snap client: %s", str(e))
+        return None
+
+    if not snap_client.present:
+        logger.warning("snap %s not installed", snap_name)
+        return None
+
+    return SnapService(snap_client, snap_service)
