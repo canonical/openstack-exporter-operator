103799731dc2e8949b6daaecb2e0c9db83592e32
Chi Wai Chan <chanchiwairay@gmail.com>
2024-03-25 15:39:05 +0800

    Add skeleton zaza test.

---
+++ tests/integration/charm_tests/openstack_exporter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Copyright 2024 Canonical
+#
+# See LICENSE file for licensing details.
+
+import logging
+import unittest
+
+import zaza.model as model
+
+logger = logging.getLogger(__name__)
+
+
+class OpenstackExporterBasicTest(unittest.TestCase):
+    """Basic test for openstack exporter."""
+
+    def test_01_dummy_test(self):
+        """Remove me."""
