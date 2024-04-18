60a220e52e4b21bb1cf182b5745edd1b82b7e8ad
jneo8 <james.lin@canonical.com>
2024-04-18 17:38:25 +0800

    feat: Provide cache configuration
    * Change unit test basic class to pytest and use pytest-mocker

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -3,18 +3,55 @@
 #
 # Learn more about testing at: https://juju.is/docs/sdk/testing
 
-import unittest
-
 import ops
 import ops.testing
-from charm import OpenstackExporterOperatorCharm
+import pytest
+from charm import CLOUD_NAME, OS_CLIENT_CONFIG, SNAP_NAME, OpenstackExporterOperatorCharm
 
 
-class TestCharm(unittest.TestCase):
-    def setUp(self):
+class TestCharm:
+    def setup_method(self, _):
         self.harness = ops.testing.Harness(OpenstackExporterOperatorCharm)
-        self.addCleanup(self.harness.cleanup)
+
+    def teardown_method(self, _):
+        self.harness.cleanup()
+
+    @pytest.mark.parametrize(
+        "config",
+        [
+            ({"cache": False, "cache_ttl": "100s"}),
+            ({"cache": True, "cache_ttl": "200s"}),
+        ],
+    )
+    def test_config_changed(self, config, mocker):
+        mock_get_installed_snap_service = mocker.patch("charm.get_installed_snap_service")
+        mock_snap_service = mocker.Mock()
+        mock_get_installed_snap_service.return_value = mock_snap_service
+
         self.harness.begin()
 
-    def test_null(self):
-        """Remove me."""
+        mock_get_keystone_data = mocker.Mock()
+        mock_keystone_data = mocker.Mock()
+        mock_get_keystone_data.return_value = mock_keystone_data
+        self.harness.charm._get_keystone_data = mock_get_keystone_data
+
+        mock_write_cloud_config = mocker.Mock()
+        self.harness.charm._write_cloud_config = mock_write_cloud_config
+        self.harness.add_relation("cos-agent", "openstack-exporter")
+
+        self.harness.update_config(config)
+        self.harness.charm.on.config_changed.emit()
+
+        mock_get_installed_snap_service.assert_called_with(SNAP_NAME)
+        mock_snap_service.configure.assert_called_with(
+            {
+                "cloud": CLOUD_NAME,
+                "os-client-config": str(OS_CLIENT_CONFIG),
+                "web": {"listen-address": f":{config.get('port', 9180)}"},
+                "cache": config.get("cache", False),
+                "cache-ttl": config.get("cache_ttl", "300s"),
+            }
+        )
+        mock_write_cloud_config.assert_called_with(mock_keystone_data)
+        mock_snap_service.restart_and_enable.assert_called()
+        mock_snap_service.stop.assert_not_called()
