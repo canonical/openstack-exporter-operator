34cd9309c698310a6465d5be6749d848fa2b9591
Xuhui Zhu <simon.zhu@canonical.com>
2024-08-02 18:57:34 -0400

    move to script

---
+++ scripts/pull_dashboard_and_alert_rules.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Clone the repository with sparse checkout
+git clone --sparse https://opendev.org/openstack/sunbeam-charms.git
+cd sunbeam-charms
+
+# Set sparse checkout paths
+git sparse-checkout set charms/openstack-exporter-k8s/src/grafana_dashboards charms/openstack-exporter-k8s/src/prometheus_alert_rules
+
+# Copy the necessary files to the src directory
+cp -r charms/openstack-exporter-k8s/src/grafana_dashboards ../src/
+cp -r charms/openstack-exporter-k8s/src/prometheus_alert_rules ../src/
+
+# Clean up
+cd ..
+rm -rf sunbeam-charms
