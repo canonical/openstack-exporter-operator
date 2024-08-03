d1bf2f67c5a56feca744002a212d11ba39c10452
Xuhui Zhu <simon.zhu@canonical.com>
2024-08-02 21:21:12 -0400

    fix temp dir

--- scripts/sync-from-sunbeam.sh
+++ scripts/sync-from-sunbeam.sh
@@ -4,8 +4,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # the temp directory used, within $DIR
-# omit the -p parameter to create a temporal directory in the default location
-WORK_DIR=`mktemp -d -p "$DIR"`
+WORK_DIR=$(mktemp -d)
 
 # check if tmp dir was created
 if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
